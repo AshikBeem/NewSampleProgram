c42e6181a8056cea61d8e42f1aef090a8e668601
ashikbeem <ashik123112@gmail.com>
2022-06-08 14:54:38 +0530

    Build have changes

--- src/org/test/CountofNum.java
+++ src/org/test/CountofNum.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class CountofNum {
 	public static void main(String[] args) {
+		int a=10, b=20, c;
+		c=a+b;
+		System.out.println(c);
 		System.out.println("enter the num");
          Scanner s=new Scanner(System.in);
 
